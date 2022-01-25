01bab97aba1a88c0217cf5c352b3204599a964ee
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2022-01-25 10:33:42 +0100

    test1-euler

---
+++ project_euler.js
@@ -0,0 +1,11 @@
+function multiplesOf3and5(number) {
+	let count = 0;
+	for (let i = 0; i <= number-1; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			count = count + i;
+		}
+	}
+	console.log(count);
+}
+
+multiplesOf3and5(1000);
