ad820f3213af1cb4847d5ef2fe0f7b27080c4093
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2022-02-21 10:36:37 +0100

    change with hesehustest

--- codeility.js
+++ codeility.js
@@ -9,18 +9,41 @@ function solution() {
 	// please note that element.innerText is not supported,
 	// you can use element.textContent instead.
 	let content = "";
-	$("table tr").each(function () {
-		$(this)
-			.find("td")
-			.each(function () {
-				const bgColor = $(this).css("background-color");
-				const color = $(this).css("color");
-				const display = $(this).css("display");
-				if (display !== "none" && color !== bgColor) {
-					content += $(this).html();
-				}
-			});
+	// document.querySelectorAll("table tr td").each((index, e) => {
+	// 	const bgColor = $(e).css("background-color");
+	// 	const color = $(e).css("color");
+	// 	const display = $(e).css("display");
+	// 	if (display !== "none" && color !== bgColor) {
+	// 		content += $(e).html();
+	// 	}
+	// });
+	document.querySelectorAll("table tr td").forEach((e) => {
+		//console.log('e', e);
+		const bgColor = e.style.backgroundColor;
+		console.log("bgcolor", bgColor);
+		const color = e.style.color;
+		console.log("color", color);
+		const display = e.style.display;
+		if (display !== "none" && color !== bgColor) {
+			content += e.innerHTML;
+		}
 	});
+	const table = $("table tr td");
+	if (table && table.length) {
+	}
+	// $("table tr").each(function (index, e) {
+	// 	$(e)
+	// 		.find("td")
+	// 		.each((index, e) => {
+	// 			const bgColor = $(e).css("background-color");console.log("bgcolor", bgColor);
+	// 			const color = $(e).css("color"); console.log("color", color);
+	// 			const display = $(e).css("display");
+	// 			if (display !== "none" && color !== bgColor) {
+	// 				content += $(e).html();
+	// 			}
+	// 		});
+	// });
 	return content;
 }
+
 console.log(solution());
